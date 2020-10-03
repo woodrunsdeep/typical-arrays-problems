aa5bda4bdaa84c1b36fa11b1b199864df7110096
Gleb Dmitriev <woodrunsdeep@gmail.com>
2020-10-03 21:17:43 +0300

    feat: add solution for typical-array-problems

--- src/index.js
+++ src/index.js
@@ -1,12 +1,27 @@
+reducer = (accumulator, currentValue) => accumulator + currentValue;
 
-exports.min = function min (array) {
-  return 0;
+compareNums = (a, b) => a - b;
+
+exports.min = function min(array) {
+    if (array === undefined || array.length === 0) {
+        return 0;
+    } else {
+        return array.sort(compareNums)[0];
+    }
 }
 
-exports.max = function max (array) {
-  return 0;
+exports.max = function max(array) {
+    if (array === undefined || array.length === 0) {
+        return 0;
+    } else {
+        return array.sort(compareNums)[array.length - 1];
+    }
 }
 
-exports.avg = function avg (array) {
-  return 0;
+exports.avg = function avg(array) {
+    if (array === undefined || array.length === 0) {
+        return 0;
+    } else {
+        return array.reduce(reducer) / array.length;
+    }
 }
